92a942329a7b3a02efc32ec07c89ccb2d29da5e9
KangJuSeong <wntjd3686@gmail.com>
2021-12-20 14:21:12 +0900

    success parentheses conversion

--- 2020_BLIND_KAKAO/PARENTHESESCONVERSION.py
+++ 2020_BLIND_KAKAO/PARENTHESESCONVERSION.py
@@ -1,55 +1,39 @@
-def top(stack):
-    if not len(stack):
-        return ''
-    else:
-        return stack[-1]
+def checkBalance(p):
+    cnt = 0
+    for i in p:
+        if i == '(':
+            cnt += 1
+        else:
+            cnt -= 1
+    return not(bool(cnt))
 
 def checkCorrect(p):
-    _p = list(p)
-    stack = []
-    stack.append(_p.pop())
-    while len(_p):
-        tmp = _p.pop()
-        if top(stack) == tmp:
-            stack.pop()
-        else:
-            stack.append(tmp)
-    if not len(stack):
-        return p
-    else:
-        return 0
-    
-def checkBalance(p):
-    left_cnt = 0
-    right_cnt = 0
+    cnt = 0
+    if not checkBalance(p):
+        return False
     for i in p:
-        if i == '(': left_cnt +=1
-        else: right_cnt += 1
-    if left_cnt == right_cnt:
-        return 1
-    else:
-        return 0
-
-def splitUV(p):
-    for i in range(len(p)):
-        if checkBalance(p[:i+1]):
-            u = p[:i+1]
-            v = p[i+1:]
-            return u, v
-
-def mixUV(u, v):
-    if checkCorrect(u):
-        _u, _v = splitUV(v)
-        return u + mixUV(_u, _v)
-    else:
-        _u, _v = splitUV(u)
-        return mixUV(_u, _v) + v
+        if i == '(':
+            cnt += 1
+        else: cnt -= 1
+        if cnt < 0:
+            return False
+    return True
 
 def solution(p):
-    answer = ''
-    # if p != '':
-    #     answer = checkCorrect(p)
-    #     if answer == 0:
-    u, v = splitUV(p)
-    print(mixUV(u, v))
-    return answer
+    if checkCorrect(p):
+        return p
+    
+    for i in range(2, len(p)+1, 2):
+        if checkBalance(p[:i]):
+            u, v = p[:i], p[i:]
+            break
+    if checkCorrect(u):
+        return u + solution(v)
+    else:
+        answer = '(' + solution(v) + ')'
+        for i in u[1:-1]:
+            if i== '(':
+                answer += ')'
+            else:
+                answer += '('
+        return answer
--- 2020_BLIND_KAKAO/WALLINSPECTION.py
+++ 2020_BLIND_KAKAO/WALLINSPECTION.py
@@ -1,3 +1,5 @@
+from itertools import combinations
+
 """
 n	weak	          dist	        result
 12	[1, 5, 6, 10]	  [1, 2, 3, 4]	2
@@ -13,24 +15,6 @@ N = 12
 WEAK = [1, 5, 6, 10]
 DIST = [1, 2, 3, 4]
 RESULT = 2
-
-
-def minDist(n, start, end):
-    return min(end-start, n-end-start)
-
-def caseNum(weak, n):
-    case = list()
-    """
-    한번에 n개 지점을 가는 경우
-    여러번 n-1개 지점을 가는 경우
-    """
-    for i in range(len(weak)):
-        print(i)
-
-    return 1
-
-def checkDist():
-    return 1
 
 def solution(n, weak, dist):
     answer = 0
