534e48e0effe45930d7b2494c3ffd5cbf7b841ba
KangJuSeong <wntjd3686@gmail.com>
2021-12-28 16:34:22 +0900

    success wall inspection

--- 2020_BLIND_KAKAO/WALLINSPECTION.py
+++ 2020_BLIND_KAKAO/WALLINSPECTION.py
@@ -17,12 +17,27 @@ DIST = [1, 2, 3, 4]
 RESULT = 2
 
 def solution(n, weak, dist):
-    answer = 0
-    return answer
+    W, F = len(weak), len(dist)
+    repair_lst = [()]
+    count = 0
+    dist.sort(reverse=True)
 
-if __name__ == '__main__':
-    answer = solution(N, WEAK, DIST)
-    if answer == RESULT:
-        print("OK")
-    else:
-        print("NO")
+    for can_move in dist:
+        repairs = []
+        count += 1
+
+        for i, wp in enumerate(weak): # [1 5 6 10]
+            start = wp
+            ends = weak[i:] + [n+w for w in weak[:i]]
+            can = [end % n for end in ends if end-start <= can_move]
+            repairs.append(set(can))
+        
+        cand = set()
+        for r in repairs:
+            for x in repair_lst:
+                new = r | set(x)
+                if len(new) == W:
+                    return count
+                cand.add(tuple(new))
+        repair_lst = cand
+    return -1
